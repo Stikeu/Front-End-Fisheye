8e6fd0dc057ee6791c0dbf64cb1bad60cfd45635
Stikeu <Nicolas60200@live.fr>
2022-09-01 18:24:27 +0200

    changement focus clavier

--- scripts/factories/mediaFactory.js
+++ scripts/factories/mediaFactory.js
@@ -27,13 +27,13 @@ class PhotographerVideo{
         const div = document.createElement("div");
         const img_video = document.createElement("a");
         img_video.innerHTML = `<video controls alt="${this._name}"
-        <source class="video_image" src="${this._mediaPath}" type="video/mp4">
+        <source class="video_image" src="${this._mediaPath}" type="video/mp4" tabindex="0">
         >`
         div.innerHTML = 
         `<p class="video_titre">${this._name}</p>
         <div class="image_likes" aria-label="likes">
             <span class="likes_number">${this._likes}</span>
-            <img src="assets/icons/heart.png" class="likes_heart" alt="heart icon">
+            <img src="assets/icons/heart.png" class="likes_heart" alt="heart icon" tabindex="0">
         </div>`;
         div.classList.add("contenue");
         // img_video.setAttribute("aria-label", `${title}`);
@@ -59,12 +59,12 @@ class PhotographerImage{
         const article = document.createElement("article");
         const div = document.createElement("div");
         const img_video = document.createElement("a");
-        img_video.innerHTML = `<img src="${this._mediaPath}" alt="${this._name}, closeup view" class="video_image">`
+        img_video.innerHTML = `<img src="${this._mediaPath}" alt="${this._name}, closeup view" class="video_image" tabindex="0">`
         div.innerHTML = 
         `<p class="image_titre">${this._name}</p>
         <div class="image_likes" aria-label="likes">
             <span class="likes_number">${this._likes}</span>
-            <img src="assets/icons/heart.png" class="likes_heart" alt="heart icon">
+            <img src="assets/icons/heart.png" class="likes_heart" alt="heart icon" tabindex="0">
         </div>`;
         div.classList.add("contenue");
         // img_video.setAttribute("aria-label", `${title}`);
